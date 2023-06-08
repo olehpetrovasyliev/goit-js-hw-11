808aaa156d0e1edc7d704daffcfc0760946a36f8
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-06-08 10:44:12 +0300

    all bugs fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import Notiflix from 'notiflix';
 import axios from 'axios';
 
@@ -11,52 +8,56 @@ const refs = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
-let currentPage = 1;
+let currentPage = null;
 let totalPages = 0;
+let searchQuery = null;
 
 refs.form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const searchQuery = refs.input.value.trim().toLowerCase();
-  localStorage.setItem('inputValue', searchQuery);
-  console.log(localStorage.getItem('inputValue'));
+
+  currentPage = 1;
+  searchQuery = refs.input.value.trim().toLowerCase();
+
   if (searchQuery === '') {
     return;
   }
 
   refs.gallery.innerHTML = '';
 
-  pushImages(searchQuery);
+  pushImages();
   // console.log(totalPages);
 }
 
-async function pushImages(query) {
+async function pushImages() {
   try {
     // currentPage = 1;
-    const resp = await getGallery(query, currentPage);
+    const resp = await getGallery();
     const images = resp.data.hits;
     if (images.length === 0) {
       return Notiflix.Notify.failure(
         'sorry, we have not found images for your request!'
       );
     }
+
     getMarkup(images);
     if (totalPages > 1) {
       // console.log(1);
       refs.loadMoreBtn.classList.remove('btn-hidden');
     }
-    if (currentPage === 1 && currentPage === totalPages) {
+    if (totalPages === 1) {
       Notiflix.Notify.success(
         `we found ${
           images.length * totalPages
-        } images for ${query}, here are all of them`
+        } images for ${searchQuery}, here are all of them`
       );
-    } else if (currentPage === 1) {
+      refs.loadMoreBtn.classList.add('btn-hidden');
+    } else if (currentPage === 1 && totalPages > 1) {
       Notiflix.Notify.success(
         `we found ${
           images.length * totalPages
-        } images for ${query}, here are first ${images.length} images`
+        } images for ${searchQuery}, here are first ${images.length} images`
       );
     } else if (currentPage === totalPages) {
       Notiflix.Notify.success('you have reached the last page');
@@ -69,16 +70,16 @@ async function pushImages(query) {
   }
 }
 
-async function getGallery(query, page) {
+async function getGallery() {
   const API_KEY = '37038896-cc3b3bcf0d11f1385e2f4c3a2';
   const params = new URLSearchParams({
     key: API_KEY,
-    q: query,
+    q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
     per_page: 40,
-    page: page,
+    page: currentPage,
   });
 
   const response = await axios.get(`https://pixabay.com/api/?${params}`);
@@ -120,10 +121,10 @@ function getMarkup(images) {
 
 function loadMoreImages() {
   currentPage += 1;
-  const searchQuery = localStorage.getItem('inputValue');
 
   // const searchQuery = refs.input.value.trim();
-  pushImages(searchQuery);
+  pushImages();
+
   if (currentPage === totalPages) {
     return refs.loadMoreBtn.classList.add('btn-hidden');
   }
